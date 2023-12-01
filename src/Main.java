e7bab3c6b9abd522861acb2ee24eac4e1efdac4e
HagridPlank <93927595+HagridPlank@users.noreply.github.com>
2023-12-01 13:27:17 +0100

    added input validation

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
 import days.Day;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    void downloadTxtFile() {
-        String filepath = "dayFiles/day1.txt";
-
-    }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
         do {
-            System.out.print("Choose day: ");
-            int day = s.nextInt();
-            if (day == 0) break;
-
-            String path = String.format("src/dayFiles/day%s.txt", day);
-            String className = String.format("days.Day%s", day);
-
             try {
+                System.out.print("Choose day: ");
+                int day = s.nextInt();
+                if (day == 0) break;
+
+                String path = String.format("src/dayFiles/day%s.txt", day);
+                String className = String.format("days.Day%s", day);
+
                 // Get the class
                 Class<?> c = Class.forName(className);
                 // Create an instance of the class
@@ -30,9 +25,14 @@ public class Main {
                 Day d = (Day) o;
                 System.out.printf("Part 1: %d\nPart 2: %d%n", d.doPart1(), d.doPart2());
 
-            } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
+            } catch (NoSuchMethodException | InvocationTargetException |
                      InstantiationException | IllegalAccessException e) {
                 e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                System.out.println("This day has not been completed yet or does not exist, use 0 to exit.");
+            } catch (InputMismatchException ignored) {
+                System.out.println("Invalid input, use 0 to exit.");
+                s.next();
             }
         } while(true);
     }
